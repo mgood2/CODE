e2139366b6e9e9839a12fec6cf8ce0ff224b452b
Eunmin Lee <eunmin_lee@hotmail.com>
2016-05-19 00:45:00 +0900

    added makefile for cpp and test.cpp

---
+++ BOJ/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int a, b;
+  cout << "enter number a and b" << endl;
+  cin >> a >> b;
+
+  cout << "a: " << a << endl;
+  cout << "b: " << b << endl;
+
+  return 0;
+}
+
