ed962c4e4f96a97afd794dcb9b1ff419d7e88876
Eunmin <eunmin_lee@hotmail.com>
2016-12-19 23:06:07 +0900

    Update sol.cpp

--- CFs/sol.cpp
+++ CFs/sol.cpp
@@ -3,13 +3,11 @@
 using namespace std;
 
 int main() {
-  freopen("in", "r", stdin);
-  freopen("out", "w", stdout);
   int n,m,a;
   int ans = 0;
-  scanf("%d %d %d", &n, m, a);
-  ans += ceil((double) m / (double) a) * ceil((double) n / (double) a);
-  printf("%d\n", ans);
-  
+  scanf("%d %d %d", &n, &m, &a);
+  ans = ceil((double) m / (double) a) * ceil((double) n / (double) a);
+  printf("%d", ans);
+
   return 0;
 }
